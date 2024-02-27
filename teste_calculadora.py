184ec586a478d255c2cbd7f9d6d1e548abc1b27c
Arthur Ramos <a30836177@gmail.com>
2024-02-27 10:02:06 -0300

    apo1(git-python): multiplicar implementada

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -5,10 +5,13 @@ def main():
     b = 3 
     soma = calculadora.somar(a, b)
     subtracao = calculadora.subtrair(a, b)
-    produto = calculadora.multiplicar(a,b)
-    print(f'{a} + {b} = soma')    
-    print(f'{a} - {b} = subtracao')
-    print(f'{a} * {b} = multiplicacao')
+    produto = calculadora.multiplicar(a, b)
+    quociente - calculadora.dividir(a, b)
+    print(f'{a} + {b} = {soma}')    
+    print(f'{a} - {b} = {subtracao}')
+    print(f'{a} * {b} = {produto}')
+    print(f'{a} / {b} = {quociente}')
+
 
 main()
 
@@ -20,3 +23,7 @@ def multiplicar(a, b):
 
 def soma(a, b):
     return a + b
+
+def dividir(a, b):
+    return a / b
+
