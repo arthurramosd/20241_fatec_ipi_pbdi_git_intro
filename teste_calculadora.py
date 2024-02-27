589990b26d56cf5e9178ddf1f08d55c885a05168
Arthur Ramos <a30836177@gmail.com>
2024-02-27 09:55:06 -0300

    ap01(git-python) multiplicar implementada

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -4,11 +4,19 @@ def main():
     a = 2
     b = 3 
     soma = calculadora.somar(a, b)
-    print(f'{a} + {b} = {soma}')
     subtracao = calculadora.subtrair(a, b)
-    pirnt(f'{a} - {b} = subtracao')
+    produto = calculadora.multiplicar(a,b)
+    print(f'{a} + {b} = soma')    
+    print(f'{a} - {b} = subtracao')
+    print(f'{a} * {b} = multiplicacao')
 
 main()
 
 def subtrair(a, b):
     return a - b 
+
+def multiplicar(a, b):
+    return a * b
+
+def soma(a, b):
+    return a + b
